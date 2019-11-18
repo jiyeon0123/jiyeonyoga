3cbabd0808b4e673f50c793912b9c7638884e365
jiyeon0123 <coruscant23@naver.com>
2019-11-19 01:10:48 +0900

    first commit

---
+++ contact-form-handler.php
@@ -0,0 +1,23 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'byjiyeon23@gmail.com';
+
+    $email_subject = "New From Submission";
+
+    $email_body = "User name : $name.\n".
+                     "User Email : $visitor_email.\n".
+                       "User Message: $message.\n".
+
+
+    $to = "byjiyeon23@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location; index.html");
