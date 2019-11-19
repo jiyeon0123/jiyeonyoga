4455561c2808fb6fc68379fe498923f43021afed
jiyeon0123 <57826112+jiyeon0123@users.noreply.github.com>
2019-11-20 02:17:18 +0900

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,23 +1,42 @@
 <?php
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $email_from = 'byjiyeon23@gmail.com';
-
-    $email_subject = "New From Submission";
-
-    $email_body = "User name : $name.\n".
-                     "User Email : $visitor_email.\n".
-                       "User Message: $message.\n".
-
-
-    $to = "byjiyeon23@gmail.com";
-
-    $headers = "From: $email_from \r\n";
-
-    $headers .= "Reply-To: $visitor_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location; index.html");
+session_start();
+require_once 'libs/phpmailer/PHPMailerAutoload.php';
+$errors =[];
+if(isset($_POST['name'],$_POST['email'],$_POST['message'])){
+    $fields=[
+        'name'=>$_POST['name'],
+        'email'=>$_POST['email'],
+        'message'=>$_POST['message']
+    ];
+    foreach($fields as $field=>$data){
+        if(empty($data)){
+            $errors[]='The '.$field . ' field is required.';
+        }
+    }
+    if(empty($errors)){
+        $m=new PHPMailer;
+        $m->isSMTP();
+        $m->SMTPAuth=true;
+        $m->Host='smtp.gmail.com';
+        $m->Username='someone@gmail.com';//replace with your email address
+        $m->Password='password';//replace with your password
+        $m->SMTPSecure='ssl';
+        $m->Port=465;
+        $m->isHTML();
+        $m->Subject ='Contact form Submitted';
+        $m->Body='From:'.$fields['name'].'('.$fields['email'].')<p>'.$fields['message'].'</p>';
+        $m->FromName='Contact';
+        $m->AddAddress('someone@gmail.com','Some one');
+        if ($m->send()) {
+            header('Location:thanks.php');
+            die();
+        }else{
+            $errors[]="Sorry ,Could not send email.Try again later.";
+        }
+    }
+}else{
+    $errors[]= 'Something went wrong';
+}
+$_SESSION['errors']=$errors;
+$_SESSION['fields']=$fields;
+header ('Location:index.php');
