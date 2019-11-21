f273ac86641b1cd33fbb8aceec4664cb9919a953
jiyeon0123 <57826112+jiyeon0123@users.noreply.github.com>
2019-11-21 22:29:36 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,4 +1,5 @@
 <?php
+if(!empty($_POST['send'])) {
    $name = $_POST['name'];
    $visitor_email = $_POST['email'];
    $message = $_POST['message'];
@@ -8,23 +9,16 @@
    
    $email_subject = "New Form Submission";
    
-   $email_body = "User name: $name.\n"
-                  "User Email: $visitor_email.\n".
-                     "User Message: $message.\n";
                      
                 
    $to = "c.nicolas662@gmail.com";
    
-   $headers = "From: $email_from \r\n";
+   $headers = "From:" . $name . "<". $visitor_email . ">\r\n";
    
-   $headers .= "Reply-To": $visitor_email \r\n";
-   
-   if (mail($to,$email_subject,$email_body,$headers)){
+   if (mail($to,$email_subject,$message,$headers)){
       $message = "Your message has been sent successfully";
       $type = "success";
      }
-   
-   header("Location: contact.html");
-   
-   
-   ?>
+}
+require_once "contact.php";
+?>
